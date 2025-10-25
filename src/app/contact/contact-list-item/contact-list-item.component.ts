aeedc812809be1559adb009b77f99573c3b7eff8
Phutiane <82708335+PhutianeMichael@users.noreply.github.com>
2025-10-25 17:06:12 +0200

    Enhance `ContactListComponent` and `ContactListItemComponent`
    - Add input and output bindings to `ContactListItemComponent` for contact interaction.
    - Integrate favorite and all contacts display in `ContactListComponent` with new computed signal for favorites.
    - Update component templates with structured tables and router links.
    - Implement component-specific SCSS styles for improved UI/UX.
    - Refine global styles with additional variables and reusable element styles.

--- src/app/contact/contact-list-item/contact-list-item.component.ts
+++ src/app/contact/contact-list-item/contact-list-item.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Contact } from '../models/contact.model';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-contact-list-item',
-  imports: [],
+  imports: [
+    JsonPipe,
+  ],
   templateUrl: './contact-list-item.component.html',
   styleUrl: './contact-list-item.component.scss'
 })
 export class ContactListItemComponent {
+  @Input() contact!: Contact;
+  @Output() selectedContact = new EventEmitter<Contact>();
 
 }
--- src/app/contact/contact-list/contact-list.component.ts
+++ src/app/contact/contact-list/contact-list.component.ts
@@ -1,25 +1,32 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { ContactsService } from '../services/contacts.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Contact } from '../models/contact.model';
-import { JsonPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-contact-list',
   imports: [
-    JsonPipe,
+    RouterLink,
+    NgOptimizedImage,
   ],
   templateUrl: './contact-list.component.html',
-  styleUrl: './contact-list.component.scss'
+  styleUrl: './contact-list.component.scss',
 })
 export class ContactListComponent {
-contactsService = inject(ContactsService);
+  contactsService = inject(ContactsService);
 
-contacts = toSignal(this.contactsService.getContacts(), {initialValue: []});
+  contacts = toSignal(this.contactsService.getContacts(), {initialValue: []});
+  favouriteContacts = computed(() => this.contacts().filter(c => c.favoritesRanking && c.favoritesRanking > 0));
 
-contact = toSignal(this.contactsService.getContactById('A6rwe'), {})
+  contact = toSignal(this.contactsService.getContactById('A6rwe'), {})
 
   trackById(_index: number, item: Contact | null) {
     return item ? item.id : null;
+  }
+
+  onContactSelected(selectedContact: Contact) {
+    console.log(selectedContact);
   }
 }
