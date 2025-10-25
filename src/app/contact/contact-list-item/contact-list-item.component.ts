b06e0b2761af830053a0a35539a3829b3df843e2
Phutiane <82708335+PhutianeMichael@users.noreply.github.com>
2025-10-25 09:06:03 +0200

    Add `ContactListItemComponent` with basic setup
    - Create `ContactListItemComponent` with associated HTML, SCSS, and spec files.
    - Define a basic structure in the component template.
    - Add unit test to verify component creation.

---
+++ src/app/contact/contact-list-item/contact-list-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-list-item',
+  imports: [],
+  templateUrl: './contact-list-item.component.html',
+  styleUrl: './contact-list-item.component.scss'
+})
+export class ContactListItemComponent {
+
+}
