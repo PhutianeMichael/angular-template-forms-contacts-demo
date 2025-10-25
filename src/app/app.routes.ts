23ef739c9589bb780ca49ac4fd40f8f3b43cd24f
Phutiane <82708335+PhutianeMichael@users.noreply.github.com>
2025-10-25 08:28:40 +0200

    Add `EditContactComponent` with routing
    - Create `EditContactComponent` with associated HTML, SCSS, and spec files.
    - Update `app.routes.ts` to include routes for editing a contact.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ContactListComponent } from './contact/contact-list/contact-list.component';
+import { EditContactComponent } from './contact/edit-contact/edit-contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'contacts', component: ContactListComponent, title: 'Contacts'},
+  {path: 'contacts/edit/:id', component: EditContactComponent, title: 'Contact - edit'},
+  {path: 'contacts/edit', component: EditContactComponent, title: 'Contact - edit'},
+  {path: '', redirectTo: '/contacts', pathMatch: 'full'},
+];
